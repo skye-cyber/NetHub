6397c8e7654ae2825eb10c130d86300b93ae2928
skye <swskye17@gmail.com>
2026-04-12 14:48:35 +0300

    Seperate APMnager from system logic to act as a services

--- Server/hotspotmanager/cli/initializer.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-
-# Get the absolute path to the parent directory
-parent_dir = str(Path(__file__).resolve().parent.parent)
-if parent_dir not in sys.path:
-    sys.path.append(parent_dir)
